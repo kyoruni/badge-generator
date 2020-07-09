c13a5232a2eaab7d2ed4a38431636be507e21e09
kyoruni <kyoruni@gmail.com>
2020-07-09 19:03:43 +0900

    :wrench: 入力フォーム用のコンポーネント作成

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -1,22 +1,15 @@
 <template>
   <div class="generator">
-    <v-select :items="icons" label="アイコン" outlined/>
-    <v-text-field label="左側のテキスト" outlined/>
-    <v-text-field label="右側のテキスト" outlined/>
-    <v-text-field label="色" outlined/>
+    <input-form/>
   </div>
 </template>
 
 <script>
+import InputForm from '@/components/Generator/InputForm'
+
 export default {
-  data () {
-    return {
-      icons: [
-        'item1',
-        'item2',
-        'item3',
-      ]
-    }
-  }
+  components: {
+    InputForm,
+  },
 }
 </script>
---
+++ src/components/Generator/InputForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="input-form">
+    <v-select :items="icons" label="アイコン" outlined/>
+    <v-text-field label="左側のテキスト" outlined/>
+    <v-text-field label="右側のテキスト" outlined/>
+    <v-text-field label="色" outlined/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      icons: [
+        'item1',
+        'item2',
+        'item3',
+      ]
+    }
+  }
+}
+</script>
