beeae9196884d9132b05cb8491377e7ade314331
kyoruni <kyoruni@gmail.com>
2020-07-09 17:47:27 +0900

    :wrench: ヘッダー用のコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <header-menu/>
     hoge
   </div>
 </template>
 
 <script>
+import HeaderMenu from '@/components/HeaderMenu'
+
 export default {
   name: 'App',
-};
+  components: {
+    HeaderMenu,
+  }
+}
 </script>
