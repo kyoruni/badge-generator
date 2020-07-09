5d6e0a5153d982be382e91d88343de013de6bc03
kyoruni <kyoruni@gmail.com>
2020-07-09 19:40:29 +0900

    :wrench: プレビューコンポーネント作成

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="generator">
     <input-form/>
+    <preview-icon/>
   </div>
 </template>
 
 <script>
 import InputForm from '@/components/Generator/InputForm'
+import PreviewIcon from '@/components/PreviewIcon'
 
 export default {
   components: {
     InputForm,
+    PreviewIcon,
   },
 }
 </script>
---
+++ src/components/PreviewIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="preview-icon">
+    <div class="text-center mt-2">
+      <img src="https://img.shields.io/badge/PHP-ccc.svg?logo=php&style=flat" height="40">
+    </div>
+  </div>
+</template>
