ff757067b92b7e4d87c8a44a7da3d93f72cf3548
kyoruni <kyoruni@gmail.com>
2020-07-09 18:14:47 +0900

    :wrench: ジェネレーターのコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <header-menu/>
+    <generator/>
     hoge
   </div>
 </template>
 
 <script>
 import HeaderMenu from '@/components/HeaderMenu'
+import Generator from '@/components/Generator'
 
 export default {
   name: 'App',
   components: {
     HeaderMenu,
+    Generator,
   }
 }
 </script>
---
+++ src/components/Generator.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="generator">
+    generator
+  </div>
+</template>
