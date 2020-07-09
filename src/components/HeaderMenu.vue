d6d61b7294840144252ee26aa2006d9c54d83fa6
kyoruni <kyoruni@gmail.com>
2020-07-09 18:09:14 +0900

    :wrench: ヘッダー作成

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="header-menu">
-    header
-  </div>
+  <v-app-bar dark flat :color="mainColor">
+    <v-toolbar-title>
+      Badge Generator
+    </v-toolbar-title>
+  </v-app-bar>
 </template>
+
+<script>
+export default {
+  computed: {
+    mainColor () {
+      return '#93b6e8'
+    }
+  }
+}
+</script>
