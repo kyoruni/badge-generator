6edd84c90ba94cc43dc2c1778cc7433ef8893d38
kyoruni <kyoruni@gmail.com>
2020-07-10 23:38:20 +0900

    :wrench: Aboutページ作成

---
+++ src/components/About.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="about">
+    <v-dialog v-model="dialog" scrollable width="800">
+      <v-card>
+        <v-card-title primary-title>
+          Badge Generator
+        </v-card-title>
+        <v-card-text>
+          <!-- 説明文 -->
+          <h3 class="mb-2">★ あばうと</h3>
+          <ul class="mb-7">
+            <li>バッジジェネレーター</li>
+            <li><a href="https://shields.io/" target="_blank" title="Shields.io">Shields.io</a>のバッジをぽちぽち簡単に作成するツール</li>
+            <li>READMEに張り付けるなどして、ドキュメントをいい感じにしよう！</li>
+            <li>自分用に作成したため、変なところがあったらすみません</li>
+          </ul>
+          <h3 class="mb-2">★ つくった人</h3>
+          <ul class="mb-7">
+            <li>きょるに @kyoruni</li>
+            <li>豆知識：左テキストを空にすると、一色のバッジになります</li>
+          </ul>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+  methods: {
+    open() {
+      this.dialog = true
+    },
+  }
+}
+</script>
--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -4,15 +4,30 @@
       <v-toolbar-title>
         Badge Generator
       </v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon @click="aboutButton()">
+        <v-icon>fas fa-question-circle</v-icon>
+      </v-btn>
+      <about ref="dialog"/>
     </v-app-bar>
   </div>
 </template>
 
 <script>
+import About from '@/components/About'
+
 export default {
   computed: {
     mainColor () {
       return '#93b6e8'
+    }
+  },
+  components: {
+    About,
+  },
+  methods: {
+    aboutButton () {
+      this.$refs.dialog.open()
     }
   }
 }
